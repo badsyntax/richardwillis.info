ef4ed0923d6ee5861dbfd2feeef0fa1b773bcae3
Richard Willis <willis.rh@gmail.com>
2021-01-09 11:33:31 +0000

    Image lightbox improvements

--- src/features/layout/Image/Image.tsx
+++ src/features/layout/Image/Image.tsx
@@ -9,24 +9,31 @@ export type ImageProps = Omit<
     HTMLImageElement
   >,
   'loading'
->;
-
-export const Image: React.FunctionComponent<ImageProps> = ({
-  src,
-  width,
-  alt,
-  ...props
-}) => {
-  if (!src) {
-    return null;
-  }
-  const type = getImageType(src);
-  const size = width ? getImageSize(Number(width)) : SIZE.XL;
-  const resizedSrc = getResizedUrl(src, type, size);
-  return (
-    <picture>
-      <SourceFormats src={src} size={size} origType={type} />
-      <img {...props} width={width} alt={alt} src={resizedSrc} loading="lazy" />
-    </picture>
-  );
+> & {
+  onLoad?: () => void;
 };
+
+export const Image = React.forwardRef<HTMLImageElement, ImageProps>(
+  ({ src, width, alt, ...props }, imageRef) => {
+    if (!src) {
+      return null;
+    }
+    const type = getImageType(src);
+    const size = width ? getImageSize(Number(width)) : SIZE.XL;
+    const resizedSrc = getResizedUrl(src, type, size);
+
+    return (
+      <picture>
+        <SourceFormats src={src} size={size} origType={type} />
+        <img
+          loading="lazy"
+          {...props}
+          width={width}
+          alt={alt}
+          src={resizedSrc}
+          ref={imageRef}
+        />
+      </picture>
+    );
+  }
+);
