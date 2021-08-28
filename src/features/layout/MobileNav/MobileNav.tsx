10878f7d2b4065df7207a5c2b9479681ddc9809f
Richard Willis <willis.rh@gmail.com>
2021-08-28 11:33:31 +0100

    Improve mobile support and typeface

---
+++ src/features/layout/MobileNav/MobileNav.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
+import { MdClose, MdMenu } from 'react-icons/md';
+
+import { Nav } from '../Nav/Nav';
+
+import STYLES from './MobileNav.module.scss';
+import { Fragment } from 'react';
+const classes = classNames.bind(STYLES);
+
+const OpenMenuButton: React.FC<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & {
+    isVisible: boolean;
+  }
+> = ({ isVisible, ...props }) => {
+  return (
+    <button className={classes('menu-button')} aria-expanded="false" {...props}>
+      <span className={classes('menu-button-label')}>Open main menu</span>
+      {isVisible ? <MdClose /> : <MdMenu />}
+    </button>
+  );
+};
+
+export const MobileNav: React.FC = () => {
+  const [isMobileNavVisible, setIsMobileNavVisible] = useState<boolean>(false);
+  const onMenuButtonClick = () => setIsMobileNavVisible((value) => !value);
+  return (
+    <Fragment>
+      <OpenMenuButton
+        onClick={onMenuButtonClick}
+        isVisible={isMobileNavVisible}
+      />
+      <Nav
+        className={classes(
+          'mobile-nav',
+          isMobileNavVisible && 'mobile-nav-visible'
+        )}
+      />
+    </Fragment>
+  );
+};
