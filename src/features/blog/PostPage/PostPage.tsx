f647437c5f3563de488906d1221d06f1e2ca2409
Richard Willis <willis.rh@gmail.com>
2021-08-29 05:57:21 +0100

    Various UI fixes

--- src/features/blog/PostPage/PostPage.tsx
+++ src/features/blog/PostPage/PostPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { PageShell } from '../../layout/PageShell/PageShell';
+import { Typography } from '../../layout/Typography/Typography';
 import { PostBody } from '../PostBody/PostBody';
 import { PostHeader } from '../PostHeader/PostHeader';
 import { Post } from '../types';
@@ -15,6 +16,7 @@ export const PostPage: React.FC<PostPagePros> = ({ post }) => {
   return (
     <PageShell title={`${post.title} - Blog`} description={post.excerpt}>
       <PostHeader title={post.title} date={post.date} author={post.author} />
+      <Typography as="hr" />
       {post.contentHtml && <PostBody content={post.contentHtml} />}
       {/* <PostComments comments={post.comments} slug={post.slug} /> */}
     </PageShell>
