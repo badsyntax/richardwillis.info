1bd1b6474892852a4459f52e4a8b356f049f21fc
Richard Willis <willis.rh@gmail.com>
2021-09-01 23:10:06 +0100

    Add favicons

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Document, { Html, Main, NextScript, Head } from 'next/document';
+
+export default class MyDocument extends Document {
+  render(): React.ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="theme-color" content="#000000" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
