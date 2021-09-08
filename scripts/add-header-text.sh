30f460846c7c0222a757de195930bdf4d4f57e78
Richard Willis <willis.rh@gmail.com>
2021-09-08 11:13:52 +0100

    Add header text to html source

---
+++ scripts/add-header-text.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+header_text=$1
+root_dir=$2
+
+DATE_GENERATED=$(date)
+export DATE_GENERATED
+
+find "$root_dir" -type f -name '*.html' | while read -r file; do
+  envsubst <"$header_text" | cat - "$file" >temp && mv temp "$file"
+done
