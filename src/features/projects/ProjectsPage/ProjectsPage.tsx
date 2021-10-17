b975909b42ffd3d6ef4e22557b54e7ec7c3fa6c2
Richard Willis <willis.rh@gmail.com>
2021-10-17 07:18:57 +0100

    Consume new endpoints & cleanup

--- src/features/projects/ProjectsPage/ProjectsPage.tsx
+++ src/features/projects/ProjectsPage/ProjectsPage.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
-import classNames from 'classnames/bind';
+import { MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 import { PageShell } from '../../layout/PageShell/PageShell';
-import { Typography } from '../../layout/Typography/Typography';
-import { Link } from '../../layout/Link/Link';
 import { ProjectsList } from '../ProjectsList/ProjectsList';
-import { Project } from '../../api/strapi';
-
-import STYLES from './ProjectsPage.module.scss';
-const classes = classNames.bind(STYLES);
+import { AboutpageSeo, Project } from '../../api/strapi';
+import { MarkdownContent } from '../../blog/MarkdownContent/MarkdownContent';
 
 export interface ProjectsPageProps {
   allProjects: Project[];
+  mdxSource: MDXRemoteSerializeResult<Record<string, unknown>>;
+  seo: AboutpageSeo;
 }
 
-export const ProjectsPage: React.FC<ProjectsPageProps> = ({ allProjects }) => {
+export const ProjectsPage: React.FC<ProjectsPageProps> = ({
+  allProjects,
+  mdxSource,
+  seo,
+}) => {
   return (
-    <PageShell
-      title="Projects"
-      description="Open-source projects by Richard Willis"
-    >
-      <Typography as="div" variant="prose" className={classes('content')}>
-        <h1>Projects</h1>
-        <p>
-          I enjoy working with web related tech like TypeScript, Node.js, Python
-          &amp; Java. I try to contribute to Open Source where I can, and mostly
-          all of my personal code is released as OSS.
-        </p>
-        <p>
-          Below are a few of the projects I&apos;ve been working on. You can
-          find all my projects{' '}
-          <Link href="https://github.com/badsyntax">on GitHub</Link>.
-        </p>
-      </Typography>
+    <PageShell title={seo.metaTitle} description={seo.metaDescription}>
+      <h1>{seo.metaTitle}</h1>
+      <MarkdownContent mdxSource={mdxSource} />
       <ProjectsList projects={allProjects} />
     </PageShell>
   );
